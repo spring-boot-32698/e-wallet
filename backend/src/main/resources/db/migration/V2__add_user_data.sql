b119ddb113d47b8a5c29d7fce91553a132a3fb75
thinhvn96 <vungocthinhbk@gmail.com>
2024-07-13 14:18:34 +0700

    update

--- backend/src/main/resources/db/migration/V2__add_user_data.sql
+++ backend/src/main/resources/db/migration/V2__add_user_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO public.user (id, first_name, last_name, username, email, "password")
-VALUES (1, 'John', 'Doe', 'johndoe', 'john@doe.com', '$2a$10$MMOkMuO8zVcXl8YH2GrZSOYf/9zeC/sznGHRVzAq0T8.tzet7QJWq');
+VALUES (1, 'John', 'Doe', 'johndoe', 'john@doe.com', '$2a$10$JRdX.mit5SeYWUynp/gwfOzerA8r.Ot3fxnMsq1Fk5BeN9M/HO8xm');
 INSERT INTO public.user (id, first_name, last_name, username, email, "password")
-VALUES (2, 'Linda', 'Calvin', 'lindacalvin', 'linda@calvin.com', '$2a$10$I5hOscIFqw73AU2/my0H0.vHAjI/rxXGcI49PB/jl8krTcM7VqkCy');
+VALUES (2, 'Linda', 'Calvin', 'lindacalvin', 'linda@calvin.com', '$2a$10$JRdX.mit5SeYWUynp/gwfOzerA8r.Ot3fxnMsq1Fk5BeN9M/HO8xm');
 INSERT INTO public.user (id, first_name, last_name, username, email, "password")
-VALUES (3, 'Jeffrey', 'Taylor', 'jeffreytaylor', 'jeffrey@taylor.com', '$2a$10$0grDMvQ7mSRLDAS6zuGOp.0ycwhgAzyE2FgLHzCV8KaXXP2TtGJ/W');
+VALUES (3, 'Jeffrey', 'Taylor', 'jeffreytaylor', 'jeffrey@taylor.com', '$2a$10$JRdX.mit5SeYWUynp/gwfOzerA8r.Ot3fxnMsq1Fk5BeN9M/HO8xm');
 
 SELECT setval('user_seq', max(id)) FROM public.user;
